9599b7a8aee15d6b72c65e8e523194751236025d
bastianmoral <bastihc@gmail.com>
2022-09-28 00:22:05 -0300

    First Blood

---
+++ Sección 3/Race.js
@@ -0,0 +1,7 @@
+// EJERCICIO CON LA FINALIDAD DE DARNOS A ENTENDER COMO FUNCIONAN LAS FUNCIONES ASYNCRONAS
+
+setTimeout(() => console.log('🐇 r finishes!'), 1500);
+
+
+console.log('🐢t finishes!')
+
