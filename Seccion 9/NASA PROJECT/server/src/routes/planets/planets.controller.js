0ce2561bba8dad65e255d93ad8815ab341c32a51
bastianmoral <bastihc@gmail.com>
2022-11-04 17:00:51 -0300

    adding cors and first get

---
+++ Seccion 9/NASA PROJECT/server/src/routes/planets/planets.controller.js
@@ -0,0 +1,11 @@
+const planets = require('../../models/planets.model')
+
+function getAllPlanets(req, res) {
+    return res.status(200).json(planets); //el status es opcional peor es buena práctica agregarlo, agregar el return nos hace que la función solo se aplique una vez. 
+
+}
+
+
+module.exports = {
+    getAllPlanets,
+};
