ea5ae9431e4fe386862d84ffa155f1e4699c02e1
bastianmoral <bastihc@gmail.com>
2022-11-05 16:15:49 -0300

    kepler data added

--- Seccion 9/NASA PROJECT/server/src/routes/planets/planets.controller.js
+++ Seccion 9/NASA PROJECT/server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets = require('../../models/planets.model')
+const { planets }  = require('../../models/planets.model')
 
 function getAllPlanets(req, res) {
     return res.status(200).json(planets); //el status es opcional peor es buena práctica agregarlo, agregar el return nos hace que la función solo se aplique una vez. 
