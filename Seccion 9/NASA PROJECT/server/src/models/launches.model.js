05df9c5a2c91d582b90d399a51812df23b54669c
bastianmoral <bastihc@gmail.com>
2022-11-08 23:47:32 -0300

    Adding launch model to NASA PROJECT

---
+++ Seccion 9/NASA PROJECT/server/src/models/launches.model.js
@@ -0,0 +1,21 @@
+
+const launches = new Map();
+
+const launch = {
+    flightNumber: 100,
+    mission: 'first name exploration',
+    Rocket: 'Expolerer name',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'kepler-422 b',
+    customer: ['ZTM','NASA','APLAPLAC'],
+    upcoming: true,
+    sucess: true,
+};
+
+
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+    launches,
+}
