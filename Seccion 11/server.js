6358affb416309c4a4254a037fab4c31aa74af41
bastianmoral <bastihc@gmail.com>
2022-12-24 01:09:13 -0300

    último detalle con Clusters

--- Seccion 11/server.js
+++ Seccion 11/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
@@ -12,15 +14,27 @@ function delay(duration) {
 
 
 app.get('/', (req, res) =>  {
-    res.send('Performance Example');
+    //JSON.stringify({}) => "{}"
+    //JSON.parse("{}") => {} 
+    //[5,4,7,8,9].sort()
+    res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
     delay(9000);
-    //delay the response
-    res.send('Ding Ding Ding!');
+    res.send(`Ding Ding Ding! ${process.pid}`);
 });
 
 
-app.listen(3000);
-
+console.log('Running server.js ....');
+//todo esto se va a ejecutar una sola vez y cuadno se incia el server
+if (cluster.isMaster) {
+    console.log('Master has been started...');
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+        cluster.fork();
+    } 
+} else {
+    console.log('Worker process started');
+    app.listen(3000);
+}
